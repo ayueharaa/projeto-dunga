099e6a73ff9444d15a264aea2c3506fbc98d92a8
Rafaela Ayumi Uehara <ayumi.uehara@unesp.br>
2024-10-30 21:24:36 -0300

    Alteração no nome dos titles

--- projeto-dunga/App.py
+++ projeto-dunga/App.py
@@ -5,15 +5,15 @@ import pandas as pd
 
 df = sns.load_dataset('iris')
 
-st.title("Titulo")
+st.title("Estudo de flores")
 st.write(df.head())
 
-st.subheader("Subtitulo2")
+st.subheader("Iris")
 st.write("Visualização das caracter´sticas das espécies de Iris")
 scatter_plot = sns.scatterplot(data=df, x='sepal_length', y= 'sepal_width', hue='species')
 st.pyplot(scatter_plot.figure)
 
-st.subheader("Subtitulo2")
+st.subheader("Petala")
 st.write("Distribuição do comprimento da pétala")
 hist_plot = sns.histplot(data=df, x='petal_length', hue='species', multiple='stack')
 st.pyplot(hist_plot.figure)
