3a80abf29b89fa61c7e75a282aa945f66f4e3704
Rafaela Ayumi Uehara <ayumi.uehara@unesp.br>
2024-10-30 21:09:47 -0300

    Add files via upload

---
+++ projeto-dunga/App.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import seaborn as sns
+from sklearn import datasets
+import pandas as pd
+
+df = sns.load_dataset('iris')
+
+st.title("Titulo")
+st.write(df.head())
+
+st.subheader("Subtitulo2")
+st.write("Visualização das caracter´sticas das espécies de Iris")
+scatter_plot = sns.scatterplot(data=df, x='sepal_lenght', y= 'sepal_width', hue='species')
+st.pyplot(scatter_plot.figure)
+
+st.subheader("Subtitulo2")
+st.write("Distribuição do comprimento da pétala")
+hist_plot = sns.histplot(data=df, x='petal_lenght', hue='species', multiple='stack')
+st.pyplot(hist_plot.figure)
